dcde31fc492305d64d89a763729781441ad7078e
A <A@DESKTOP-96HTVN9.mshome.net>
2022-03-28 22:49:42 +0530

    1st commit

---
+++ src/main/java/com/cloudboard/CloudboardJenkinsAwsApplication.java
@@ -0,0 +1,13 @@
+package com.cloudboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudboardJenkinsAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudboardJenkinsAwsApplication.class, args);
+	}
+
+}
